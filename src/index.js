29a57a10885b9b2344baf9c5a243d3b9555c13ac
trenysx <144817557+trenysx@users.noreply.github.com>
2026-08-24 01:10:26 +0700

    feat: add src/index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { scan } from "./scan.js";
+import { largestFiles } from "./scan.js";
+export { squarify } from "./treemap.js";
+export { renderHtml, fmtSize } from "./report.js";
+export { runCli } from "./cli.js";
+export { scan, largestFiles };
